09afcbe1f15a7ae8d07230e6288b53c2c87b92d7
fernandopaniagua <fernando.paniagua@gmail.com>
2018-11-03 20:07:23 +0100

    Initial commit

---
+++ app/src/main/java/net/juanxxiii/pmmydm_ol_sesion1/MainActivity.java
@@ -0,0 +1,20 @@
+package net.juanxxiii.pmmydm_ol_sesion1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void saludar(View v){
+        Log.d("DAM-PMMyDM-OL","HOLA A TODOS");
+
+    }
+}
